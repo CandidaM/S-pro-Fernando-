7ad014f0f608f0398ac15d097c5822ffb22a7f53
CandidaM <64532668+CandidaM@users.noreply.github.com>
2022-01-13 13:29:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+var ball;
+
+var btn1;
+var btn2;
+
+function setup() {
+  createCanvas(400,400);
+ 
+}
+
+function draw() 
+{
+  background(51);
+
+ 
+}
+
+
+function hForce()
+{
+ 
+}
+
+function vForce()
+{
+  
+}
